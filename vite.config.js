6d4eee513c5799f59fd94a2acc2f62c7cc893ee8
adhi <Harshbh20102@gmail.com>
2025-02-19 21:14:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/my-react-app/', // Replace with your repo name
-  build: {
-    outDir: 'docs', // GitHub Pages will serve from this folder
-  }
+  
 });
